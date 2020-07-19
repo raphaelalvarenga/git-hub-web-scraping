be3fcca6459aa81b4c8a0b5697f5e42d3dc18f19
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-07-19 19:57:25 -0300

    [Adapting after GitHub's changes] GitHub has changed its HTML. So the navigation got problems. Made the fix.

--- src/routines/getRowData.ts
+++ src/routines/getRowData.ts
@@ -6,19 +6,19 @@ import FolderInterface from "../interfaces/folder-interface";
 
 const getRowData = async (html: string): Promise<any[]> => {
 
-    const $ = cheerio.load(html);    
+    const $ = cheerio.load(html);
 
     // First thing here is to make a loop through the items to build the object
-    const promises: any[] = $('.files .js-navigation-item').map(async (i: number, item: CheerioElement) => {
+    const promises: any[] = $('.Box.mb-3 .js-navigation-item').map(async (i: number, item: CheerioElement) => {
 
         // This will store the instance of the loop
         let tempItem: any;
 
         // Getting item data such as its link, whether it's a file or folder, etc...
-        const svgClasses = $(item).find(".icon > svg").attr("class");
+        const svgClasses = $(item).find("svg").attr("class");
         const isFile = svgClasses?.split(" ")[1] === "octicon-file";
-        const content: Cheerio = $(item).find("td.content a");
-        const relativeLink = content.attr("href")
+        const content: Cheerio = $(item).find("span").first().find("a");
+        const relativeLink = content.attr("href");
 
         // If it is a file...
         if (isFile) {
